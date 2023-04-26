8cf819721c519f47816ec915e141a585582bb719
gayatridhote <72754354+gayatridhote@users.noreply.github.com>
2023-04-26 15:18:22 +0530

    Update Factorial of Number.cs

--- Working with text/Udemy CSharpe Exercises/Factorial of Number.cs
+++ Working with text/Udemy CSharpe Exercises/Factorial of Number.cs
@@ -1,4 +1,6 @@
-﻿using System;
+/*Write a program and ask the user to enter a number. Compute the factorial of the number and print it on the console.
+For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.*/
+using System;
 using System.Collections.Generic;
 using System.Text;
 
