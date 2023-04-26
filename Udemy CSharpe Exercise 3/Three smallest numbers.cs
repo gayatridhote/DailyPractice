4ca6e6637c2e9fe48065098d19c8a581fad0c1c2
gayatridhote <72754354+gayatridhote@users.noreply.github.com>
2023-04-26 15:35:10 +0530

    Update Three smallest numbers.cs

--- Udemy CSharpe Exercise 3/Three smallest numbers.cs
+++ Udemy CSharpe Exercise 3/Three smallest numbers.cs
@@ -1,4 +1,6 @@
-﻿using System;
+/*Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10).
+If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display the 3 smallest numbers in the list.*/
+using System;
 using System.Collections.Generic;
 using System.Text;
 
