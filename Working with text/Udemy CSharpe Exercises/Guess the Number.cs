d9566db816babf69904cfaa9e0f48f3ea544218e
gayatridhote <72754354+gayatridhote@users.noreply.github.com>
2023-04-26 15:19:42 +0530

    Update Guess the Number.cs

--- Working with text/Udemy CSharpe Exercises/Guess the Number.cs
+++ Working with text/Udemy CSharpe Exercises/Guess the Number.cs
@@ -1,4 +1,7 @@
-﻿using System;
+/*Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number. 
+If the user guesses the number, display “You won"; otherwise, display “You lost".
+(To make sure the program is behaving correctly, you can display the secret number on the console first.)*/
+using System;
 using System.Collections.Generic;
 using System.Text;
 
