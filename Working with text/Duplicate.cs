3d8b03319d605d99192c1a7b8b689a4f7af923f4
gayatridhote <72754354+gayatridhote@users.noreply.github.com>
2023-04-26 15:11:41 +0530

    Update Duplicate.cs

--- Working with text/Duplicate.cs
+++ Working with text/Duplicate.cs
@@ -1,4 +1,6 @@
-﻿using System;
+/*Write a program and ask the user to enter a few numbers separated by a hyphen. If the user simply presses Enter, without supplying an input,
+exit immediately; otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console.*/
+using System;
 using System.Collections.Generic;
 using System.Text;
 
