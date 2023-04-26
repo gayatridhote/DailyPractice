aa9ae22afe9262712d075281df32053e03a2fe7c
gayatridhote <72754354+gayatridhote@users.noreply.github.com>
2023-04-26 15:33:51 +0530

    Update Displaying only Unique Numbers.cs

--- Udemy CSharpe Exercise 3/Displaying only Unique Numbers.cs
+++ Udemy CSharpe Exercise 3/Displaying only Unique Numbers.cs
@@ -1,4 +1,7 @@
-﻿using System;
+/* Write a program and ask the user to continuously enter a number or type "Quit" to exit. 
+The list of numbers may include duplicates. 
+Display the unique numbers that the user has entered.*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
