8a85842ffe5e6d67312ccd5f37510faaac481cb9
gayatridhote <72754354+gayatridhote@users.noreply.github.com>
2023-04-26 15:31:21 +0530

    Update Reverse User Name.cs

--- Udemy CSharpe Exercise 3/Reverse User Name.cs
+++ Udemy CSharpe Exercise 3/Reverse User Name.cs
@@ -1,4 +1,8 @@
-﻿using System;
+/*Write a program and ask the user to enter their name. 
+Use an array to reverse the name and then store the result in a new string. 
+Display the reversed name on the console.
+*/
+using System;
 using System.Collections.Generic;
 using System.Text;
 
